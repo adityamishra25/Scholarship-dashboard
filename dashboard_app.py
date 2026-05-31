52f750bc19982aa8b00f3848b95cbbc3035d8bd1
Aditya Mishra <93214368+adityamishra25@users.noreply.github.com>
2026-05-31 10:01:52 +0530

    Update dashboard_app.py

--- dashboard_app.py
+++ dashboard_app.py
@@ -13,7 +13,7 @@ DATA_PATH = "master_dataset.csv"
 if not os.path.exists(DATA_PATH):
     gdown.download(
         "https://drive.google.com/uc?id=1qfqIGGDKBfrBo0sEN1AwiyOFSFvk6iYN",
-        DATA_PATH, quiet=False, fuzzy=True
+        DATA_PATH, quiet=False
     )
 
 df = pd.read_csv(DATA_PATH)
